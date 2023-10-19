f2aed9f4385209198e24efed488243d407d4f425
Ursa1337 <mrertert@gmail.com>
2023-10-20 00:51:35 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-function toReadable (number) {
-  number = number.toString().split('');
+module.exports = function toReadable (number) {
+  let result = [];
+  number = number.toString().split('').map(Number);
   const numList = [
     'zero',
     'one',
@@ -35,11 +36,38 @@ function toReadable (number) {
     'ninety'
   ];
   const numEachDec = [
-    'Hundred',
-    'Thousand'
+    'hundred',
+    'thousand'
   ];
 
-  console.log(number.length > numEachDec.length + 2);
+  while (number.length > 0) {
+    if (number.length <= numEachDec.length + 2) {
+      if (number.length >= 3) {
+        result.push(numList[number.shift()]);
+        result.push(numEachDec[number.length - 2]);
+      }
+      else {
+        const num = Number(number.join(''));
+        if (num === 0) {
+          if (result.length === 0) {
+            result.push(numList[0]);
+          }
+          number.splice(0, 2);
+        }
+        else if (num < 20) {
+          result.push(numList[num]);
+          number.splice(0, 2);
+        }
+        else {
+          const [dec, digit] = [number.shift(), number.shift()];
+          result.push(numDecimal[dec]);
+          (digit) && result.push(numList[digit]);
+        }
+      }
+    }
+    else {
+      throw 'Out of range';
+    }
+  }
+  return result.join(' ');
 }
-
-toReadable(9008)
