130d88607b0db1cd1c8180481ad1d844e907829a
Ursa1337 <mrertert@gmail.com>
2023-10-19 23:50:53 +0300

    init: initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function toReadable (number) {
-  
+function toReadable (number) {
+  number = number.toString().split('');
+  const numList = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
+  ];
+  const numDecimal = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
+  ];
+  const numEachDec = [
+    'Hundred',
+    'Thousand'
+  ];
+
+  console.log(number.length > numEachDec.length + 2);
 }
+
+toReadable(9008)
